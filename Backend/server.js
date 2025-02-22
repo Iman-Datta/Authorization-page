d85bf427e4d5c83ad90bec22eb74e756c0c341bb
Iman Datta <dattaiman56@gmail.com>
2025-02-22 08:57:02 +0530

    comant out

--- Backend/server.js
+++ Backend/server.js
@@ -11,9 +11,6 @@ app.use(bodyParser.json());
 let users = [];
 let userId = 1;
 
-// API Routes
-
-// Create Account
 app.post("/register", (req, res) => {
     const { username, firstName, lastName, password } = req.body;
 
@@ -26,14 +23,13 @@ app.post("/register", (req, res) => {
         username,
         firstName,
         lastName,
-        password, // Ideally, hash this before storing
+        password,
     };
 
     users.push(newUser);
     res.status(201).json({ message: "Account created successfully!" });
 });
 
-// Login
 app.post("/login", (req, res) => {
     const { username, password } = req.body;
     const user = users.find(user => user.username === username);
@@ -48,7 +44,6 @@ app.post("/login", (req, res) => {
     res.json({ message: `Welcome back, ${user.firstName}!` });
 });
 
-// Show all accounts
 app.get("/users", (req, res) => {
     res.json(users.map(user => ({
         id: user.id,
@@ -57,7 +52,6 @@ app.get("/users", (req, res) => {
     })));
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
